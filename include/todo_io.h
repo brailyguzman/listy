e2440ff584804ee1be6d935456efa2212ab2e588
Braily Guzman <94770717+brailyguzman@users.noreply.github.com>
2024-09-12 12:40:41 -0400

    Implement get_todo_path and ensure_directory_exists

--- include/todo_io.h
+++ include/todo_io.h
@@ -5,6 +5,8 @@
 
 const char* TODO_PATH;
 
+void ensure_directory_exists(const char*);
+const char* get_todo_path();
 int count_lines();
 
 #endif
--- src/todo_io.c
+++ src/todo_io.c
@@ -1,16 +1,69 @@
 #include "../include/todo_io.h"
 
-const char* TODO_PATH = "~/listy/todo.txt";
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+
+void ensure_directory_exists(const char* path) {
+	char* dir_name = strdup(path);
+
+	if (dir_name == NULL) {
+		perror("Error duplicating path");
+		return;
+	}
+
+	char* last_slash = strrchr(dir_name, '/');
+
+	if (last_slash == NULL) {
+		perror("Error locating the last occurrence of the forward slash");
+		return;
+	}
+
+	*last_slash = '\0';
+
+	if (mkdir(dir_name, 0755) != 0) {
+		perror("Error creating directory");
+		return;
+	}
+
+	free(dir_name);
+}
+
+const char* get_todo_path() {
+	const char* home = getenv("HOME");
+
+	if (home == NULL) {
+		perror("Error getting home directory");
+		return NULL;
+	}
+
+	char* full_path = malloc(strlen(home) + strlen("/listy/todo.txt") + 1);
+
+	if (full_path == NULL) {
+		perror("Error allocating memory for the todo path");
+		return NULL;
+	}
+
+	strcpy(full_path, home);
+	strcat(full_path, "/listy/todo.txt");
+
+	return full_path;
+}
 
 int count_lines() {
+	const char* full_path = get_todo_path();
+	printf("%s\n", full_path);
+
 	FILE* fptr;
-	fptr = fopen(TODO_PATH, "r+");
+	fptr = fopen(full_path, "r");
 
 	if (fptr == NULL) {
-		fptr = fopen(TODO_PATH, "w+");
+
+        ensure_directory_exists(full_path);
+		fptr = fopen(full_path, "w");
 
 		if (fptr == NULL) {
-			perror("Error opening file");
+			perror("Error opening file\n");
 			return -1;
 		}
 	}
@@ -25,6 +78,7 @@ int count_lines() {
 	}
 
 	fclose(fptr);
+	free((void*)full_path);
 
 	return lines;
 }
