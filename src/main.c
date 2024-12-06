3146a6a41c6fe6a6c2de40a7556c5a68e728ef3a
Braily Guzman <brailyg@outlook.com>
2024-12-05 22:29:36 -0500

    Add the functionality for running the program with the add flag

--- src/main.c
+++ src/main.c
@@ -1,15 +1,37 @@
+#include <colors.h>
+#include <listy.h>
 #include <stdio.h>
 #include <string.h>
 
 int main(int argc, char** argv) {
-	if (argc <= 1 || strcmp(argv[1], "help") == 0) {
-		// Here, we should print how to use the available commands.
+	if (argc <= 1) {
+		// Todo: Display the current todos.
+		return 0;
+	}
+
+	if (strcmp(argv[1], "help") == 0) {
+		// Todo: Display a guide on how to use the commands.
 		return 0;
 	}
 
 	if (strcmp(argv[1], "add") == 0) {
-		// TODO: Implement add todo functionality
+		char text[100];
+		printf("%s[?]%s Enter Todo: ", YEL, RESET);
+
+		if (fgets(text, sizeof(text), stdin) != NULL) {
+			int result = add(text);
+
+			if (result == -1) {
+				return -1;
+			}
+
+			printf("%s[+]%s Added Todo!\n", GRN, RESET);
+
+			return 0;
+		}
 	}
+
+	// TODO: Finish implement CRUD for Todos.
 
 	return 0;
 }
