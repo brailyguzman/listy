22a0c7503d217bfcf05fd9976f1968812a3e0cb0
Braily Guzman <brailyg@outlook.com>
2024-12-26 15:58:22 -0500

    Implement help flag functionality and make the control flows a bit more organized

--- src/main.c
+++ src/main.c
@@ -3,24 +3,39 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct {
+	char* title;
+	char* description;
+} Command;
+
+void help() {
+	Command commands[] = {
+		{.title = "no flag", .description = "Shows the available todos."},
+		{.title = "add <text>",
+		 .description = "Adds a new todo to the list with the given text."},
+		{.title = "remove <id>",
+		 .description = "Removes the todo with the given ID from the list."},
+		{.title = "done <id>",
+		 .description = "Marks the todo with the given ID as complete."},
+		{.title = "clear", .description = "Removes all todos from the list."}};
+
+	size_t length = sizeof(commands) / sizeof(commands[0]);
+
+	for (size_t i = 0; i < length; i++) {
+		Command command = commands[i];
+		printf("%s%s%s\n  - %s%s%s\n", GRN, BOLD, command.title, RESET, CYN,
+			   command.description);
+	}
+}
+
 int main(int argc, char** argv) {
 	if (argc <= 1) {
 		view_todos();
-		return 0;
-	}
-
-	if (strstr(argv[1], "help")) {
-		// TODO: Help command should display available commands.
-		return 0;
-	}
-
-	if (strstr(argv[1], "clear")) {
-		// TODO: Run clear function
+	} else if (strstr(argv[1], "help")) {
+		help();
+	} else if (strstr(argv[1], "clear")) {
 		clear_todos();
-		return 0;
-	}
-
-	if (strstr(argv[1], "add")) {
+	} else if (strstr(argv[1], "add")) {
 		if (argv[2] != NULL) {
 			int result = add_todo(argv[2]);
 
@@ -39,11 +54,7 @@ int main(int argc, char** argv) {
 				}
 			}
 		}
-
-		return 0;
-	}
-
-	if (strstr(argv[1], "remove")) {
+	} else if (strstr(argv[1], "remove")) {
 		if (argv[2] != NULL) {
 			int id = atoi(argv[2]);
 
@@ -58,9 +69,7 @@ int main(int argc, char** argv) {
 			fprintf(stderr,
 					"Error: Missing ID argument for 'remove' command.\n");
 		}
-	}
-
-	if (strstr(argv[1], "done")) {
+	} else if (strstr(argv[1], "done")) {
 		if (argv[2] != NULL) {
 			int id = atoi(argv[2]);
 
@@ -75,6 +84,9 @@ int main(int argc, char** argv) {
 			fprintf(stderr,
 					"Error: Missing ID argument for 'remove' command.\n");
 		}
+	} else {
+		printf("%sInvalid flag%s\n\n", RED, RESET);
+		help();
 	}
 
 	return 0;
